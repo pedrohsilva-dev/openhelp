7fa43c28c11a41992c63b91cb1a602c75942d15c
pedro henrique <pedro@debian.pedro>
2022-11-16 20:37:48 -0300

    jsdbin

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-import os
-from app import create_app
-import config
+# import os
+# from app import create_app
+# import config
 
-app = create_app(config.environment[
-    os.environ.get("FLASK_ENV")
- ])
+# app = create_app(config.environment[
+#     os.environ.get("FLASK_ENV")
+#  ])
+
+
+from app import create_app
+from config import ProdConfig
+
+
+app = create_app(ProdConfig)
+
+app.run("localhost", 8080, False, True)
